f2edaec24fdff5258357c09ba87d79bb35731160
johnerler <erler.john@gmail.com>
2018-10-11 20:43:56 -0500

    routes controllers and views

--- app/controllers/api/vintage_guitars_controller.rb
+++ app/controllers/api/vintage_guitars_controller.rb
@@ -1,2 +1,11 @@
 class Api::VintageGuitarsController < ApplicationController
+  def vintage_guitar_method
+    @vintage_guitars = VintageGuitar.all
+    render "vintage_guitar.json.jbuilder"
+  end
+
+  def vintage_guitar_gibson_method
+    @vintage_guitars = VintageGuitar.all
+    render "vintage_guitar.json.jbuilder"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    get "/vintage_guitar_url" => "vintage_guitars#vintage_guitar_method"
+    get "/vintage_guitar_url" => "vintage_guitars#vintage_guitar_gibson_method"
+  end
 end
